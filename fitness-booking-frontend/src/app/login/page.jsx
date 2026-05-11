ef4c011e56374e5c06caeb0ae0345a0fe1459888
stephenJoshua <149865909+stephen1234567890000000@users.noreply.github.com>
2026-05-11 15:27:01 +0700

    refactor: add localstorage for user role

--- fitness-booking-frontend/src/app/login/page.jsx
+++ fitness-booking-frontend/src/app/login/page.jsx
@@ -18,6 +18,7 @@ export default function Login() {
     try {
       const { data } = await api.post("/auth/login", form);
       localStorage.setItem("accessToken", data.accesstoken);
+      localStorage.setItem("role", data.role);
       router.push("/services");
     } catch (error) {
       setError(error.response?.data?.message);
